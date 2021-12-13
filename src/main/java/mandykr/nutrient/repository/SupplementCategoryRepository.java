8e4e5ba17879083e8d59479d2730bef05d3c0e02
mandykr <hongseokii92@gmail.com>
2021-12-13 15:45:32 +0900

    feat: 카테고리의 repository 작성

---
+++ src/main/java/mandykr/nutrient/repository/SupplementCategoryRepository.java
@@ -0,0 +1,7 @@
+package mandykr.nutrient.repository;
+
+import mandykr.nutrient.entity.SupplementCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplementCategoryRepository extends JpaRepository<SupplementCategory, Long> {
+}
