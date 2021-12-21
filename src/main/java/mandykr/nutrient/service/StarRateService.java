194bef6a8168f42ec69edf5ec1238037d0c6427c
Gwak seyeon <ksyk1205@naver.com>
2021-12-22 01:09:21 +0900

    <feat>(<StarRateService>): <별점 등록,수정 기능 추가>

--- src/main/java/mandykr/nutrient/service/StarRateService.java
+++ src/main/java/mandykr/nutrient/service/StarRateService.java
@@ -21,63 +21,66 @@ public class StarRateService {
     private final SupplementRepository supplementRepository;
 
     /**
+     * 영양제 조회
+     * @param supplementId 영양제 아이디
+     * @return
+     */
+    private Supplement getSupplement(Long supplementId) {
+        //영양제 아이디가 없다면 error
+        return supplementRepository.findById(supplementId).orElseThrow(()-> new IllegalArgumentException("not found SupplementId"));
+    }
+    /**
+     * 영양제 테이블 평점 수정
+     * @param supplement 영양제
+     */
+    private void updateSupplementRanking(Supplement supplement) {
+        List<StarRate> starRateList = starRateRepository.findBySupplement(supplement);
+        long sum=0;
+        for(StarRate starRate : starRateList){
+            sum += starRate.getStarNumber();
+        }
+        double ranking = (double) sum / starRateList.size();
+        supplement.setRanking(ranking);
+        supplementRepository.save(supplement);
+    }
+
+    /**
      * 별점 등록
      * @param supplementId //영양제 아이디
      * @param starNumber //별점 갯수
      * @return
      */
     @Transactional
-    public StarRateDto createStarRate(Long supplementId,Long starNumber){
-        //영양제 아이디가 없다면 error
-        Supplement supplement = supplementRepository.findById(supplementId).get();
-        if(supplement.getId() == null){
-            throw new IllegalArgumentException("없는 영양제 입니다.");
-        }
+    public StarRateDto createStarRate(Long supplementId,int starNumber){
+        //영양제 조회
+        Supplement supplement = getSupplement(supplementId);
 
         //영양제 별점 등록
         StarRateDto starRateDto = new StarRateDto(starRateRepository.save(new StarRate(starNumber,supplement)));
 
-        //영양제 테이블 평점 수정
-        List<StarRate> starRateList = starRateRepository.findBySupplement(supplement);
-        long sum=0;
-        for(StarRate starRate : starRateList){
-            sum += starRate.getStarNumber();
-        }
-        double ranking = (double) sum / starRateList.size();
-        supplement.setRanking(ranking);
-        supplementRepository.save(supplement);
+        //Supplement 테이블 평점 수정
+        updateSupplementRanking(supplement);
 
         return starRateDto;
     }
 
-
     /**
-     *
+     * 별점 수정
      * @param supplementId 영양제 아이디
      * @param starRateId  별점 아이디
      * @param starNumber 별점 갯수
      * @return
      */
     @Transactional
-    public StarRateDto updateStarRate(Long supplementId,Long starRateId,Long starNumber){
-        //영양제 아이디가 없다면 error
-        Supplement supplement = supplementRepository.findById(supplementId).get();
-        if(supplement.getId() == null){
-            throw new IllegalArgumentException("없는 영양제 입니다.");
-        }
+    public StarRateDto updateStarRate(Long supplementId,Long starRateId,int starNumber){
+        //영양제 조회
+        Supplement supplement = getSupplement(supplementId);
 
         //영양제 별점 수정
         StarRateDto starRateDto = new StarRateDto(starRateRepository.save(new StarRate(starRateId,starNumber,supplement)));
 
-        //영양제 테이블 평점 수정
-        List<StarRate> starRateList = starRateRepository.findBySupplement(supplement);
-        long sum=0;
-        for(StarRate starRate : starRateList){
-            sum += starRate.getStarNumber();
-        }
-        double ranking = (double) sum / starRateList.size();
-        supplement.setRanking(ranking);
-        supplementRepository.save(supplement);
+        //Supplement 테이블 평점 수정
+        updateSupplementRanking(supplement);
 
         return starRateDto;
     }
--- src/test/java/mandykr/nutrient/service/StarRateServiceTest.java
+++ src/test/java/mandykr/nutrient/service/StarRateServiceTest.java
@@ -22,27 +22,37 @@ class StarRateServiceTest {
     SupplementRepository supplementRepository;
 
     @Test
-    public void createStarRate(){
+    public void 별점_등록_성공(){
         Supplement supplement = new Supplement();
         supplement.setRanking(0.0);
         supplement.setName("test");
         Supplement supplement1 = supplementRepository.save(supplement);
 
-        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2L);
-        starRateService.createStarRate(supplement1.getId(),5L);
+        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2);
+        starRateService.createStarRate(supplement1.getId(),5);
 
         Assertions.assertThat(supplementRepository.findById(supplement1.getId()).get().getRanking()).isEqualTo(3.5);
     }
 
     @Test
-    public void updateStarRate(){
+    public void 별점_조회_실패(){
+        Supplement supplement = new Supplement();
+        supplement.setRanking(0.0);
+        supplement.setName("test");
+
+        starRateService.createStarRate(supplement.getId(),2); //영양제 없는거로 조회하니까 에러 발생
+
+    }
+
+    @Test
+    public void 별점_수정_성공(){
         Supplement supplement = new Supplement();
         supplement.setRanking(0.0);
         supplement.setName("test");
         Supplement supplement1 = supplementRepository.save(supplement);
 
-        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2L);
-        starRateService.updateStarRate(supplement1.getId(),starRateDto.getId(),5L);
+        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2);
+        starRateService.updateStarRate(supplement1.getId(),starRateDto.getId(),5);
 
         Assertions.assertThat(supplementRepository.findById(supplement1.getId()).get().getRanking()).isEqualTo(5.0);
     }
