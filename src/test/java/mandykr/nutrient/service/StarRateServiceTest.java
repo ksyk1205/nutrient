2ed921eb977cea183804c58c1f3f04452701c45f
Gwak seyeon <ksyk1205@naver.com>
2021-12-27 19:30:20 +0900

    feat: 별점Controller 기능 구현 및 Member 추가

--- src/test/java/mandykr/nutrient/service/StarRateServiceTest.java
+++ src/test/java/mandykr/nutrient/service/StarRateServiceTest.java
@@ -1,8 +1,10 @@
 package mandykr.nutrient.service;
 
 import mandykr.nutrient.dto.StarRateDto;
+import mandykr.nutrient.entity.Member;
 import mandykr.nutrient.entity.StarRate;
 import mandykr.nutrient.entity.Supplement;
+import mandykr.nutrient.repository.MemberRepository;
 import mandykr.nutrient.repository.SupplementRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,27 +22,38 @@ class StarRateServiceTest {
     StarRateService starRateService;
     @Autowired
     SupplementRepository supplementRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void 별점_등록_성공(){
         Supplement supplement = new Supplement();
         supplement.setRanking(0.0);
-        supplement.setName("test");
+        supplement.setName("testname");
         Supplement supplement1 = supplementRepository.save(supplement);
 
-        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2);
-        starRateService.createStarRate(supplement1.getId(),5);
+        Member member = new Member();
+        member.setMemberId("test");
+        member.setName("name1");
+        Member member1 = memberRepository.save(member);
 
-        Assertions.assertThat(supplementRepository.findById(supplement1.getId()).get().getRanking()).isEqualTo(3.5);
+        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2,member1);
+
+        Assertions.assertThat(supplementRepository.findById(supplement1.getId()).get().getRanking()).isEqualTo(2.0);
     }
 
     @Test
     public void 별점_조회_실패(){
         Supplement supplement = new Supplement();
         supplement.setRanking(0.0);
-        supplement.setName("test");
+        supplement.setName("testname");
 
-        starRateService.createStarRate(supplement.getId(),2); //영양제 없는거로 조회하니까 에러 발생
+        Member member = new Member();
+        member.setMemberId("test");
+        member.setName("name1");
+        Member member1 = memberRepository.save(member);
+
+        starRateService.createStarRate(supplement.getId(),2,member1); //영양제 없는거로 조회하니까 에러 발생
 
     }
 
@@ -48,11 +61,16 @@ class StarRateServiceTest {
     public void 별점_수정_성공(){
         Supplement supplement = new Supplement();
         supplement.setRanking(0.0);
-        supplement.setName("test");
+        supplement.setName("testname");
         Supplement supplement1 = supplementRepository.save(supplement);
 
-        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2);
-        starRateService.updateStarRate(supplement1.getId(),starRateDto.getId(),5);
+        Member member = new Member();
+        member.setMemberId("test");
+        member.setName("name1");
+        Member member1 = memberRepository.save(member);
+
+        StarRateDto starRateDto = starRateService.createStarRate(supplement1.getId(),2,member1);
+        starRateService.updateStarRate(supplement1.getId(),starRateDto.getId(),5,member1);
 
         Assertions.assertThat(supplementRepository.findById(supplement1.getId()).get().getRanking()).isEqualTo(5.0);
     }
