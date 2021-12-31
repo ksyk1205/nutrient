00ba42a9d112e34b2e775a9debcb2973f81da9b3
Gwak seyeon <ksyk1205@naver.com>
2021-12-31 22:55:36 +0900

    chore  : next value for hibernate_sequence

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,10 @@
-insert into member(id,`member_id`, `name`) values (1,'testMemberId1', 'martin');
-insert into member(id,`member_id`, `name`) values (2,'testMemberId2', 'dennis');
+
+
+insert into member(id,member_id,name) values (1,'testMemberId1', 'martin');
+call next value for hibernate_sequence;
+
+insert into member(id,member_id,name) values (2,'testMemberId2', 'dennis');
+call next value for hibernate_sequence;
 
 insert into SUPPLEMENT(id,name,ranking) values(1,'testName',0.0);
+call next value for hibernate_sequence;
