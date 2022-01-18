5168d696440bdb1d90aa4ff3d76d7dc2899d3bf0
mandykr <hongseokii92@gmail.com>
2022-01-18 11:50:36 +0900

    refactor: @Transactional 추가

--- src/main/java/mandykr/nutrient/service/combination/CombinationSeviceImpl.java
+++ src/main/java/mandykr/nutrient/service/combination/CombinationSeviceImpl.java
@@ -7,13 +7,16 @@ import mandykr.nutrient.repository.combination.CombinationRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CombinationSeviceImpl implements CombinationService{
     private final CombinationRepository combinationRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Page<CombinationDto> getCombinations(CombinationSearchCondition condition, Pageable pageable) {
         Page<CombinationDto> resultPage = null;
 
